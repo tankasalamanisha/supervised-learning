796675c80710219b97c11d0b77e4c1a44d3ba46c
Manisha Akshita T V <72745106+tankasalamanisha@users.noreply.github.com>
2023-05-01 14:05:41 +0000

    completed implimentation of accuracy using tp,tn,fp,fn

--- performance-metrics/metrics.py
+++ performance-metrics/metrics.py
@@ -21,9 +21,76 @@ def accuracy(y_true, y_pred):
 
 def true_positive(y_true, y_pred):
     """Function to calculate True Positive
+    Args:
+    -----------------
+    y_true: list of true/actual values.
+    y_pred: list of predicted values.
+
+    Returns:
+    -----------------
+    int: count of true positives.
     """
     tp = 0
     for yt, yp in zip(y_true, y_pred):
         if yt == 1 and yp ==1:
             tp+=1
     return tp
+def true_negative(y_true, y_pred):
+    """Function to calculate true negatives.
+    Args:
+    -----------------
+    y_true: list of true/actual values.
+    y_pred: list of predicted values.
+
+    Returns:
+    -----------------
+    int: count of true negative.
+    """
+    tn=0
+    for yt,yp in zip(y_true, y_pred):
+        if yt == 0 and yp == 0:
+            tn+=1
+    return tn
+def false_positive(y_true, y_pred):
+    """Function to calculate false positive.
+    Args:
+    -----------------
+    y_true: list of true/actual values.
+    y_pred: list of predicted values.
+
+    Returns:
+    -----------------
+    int: count of false positives.
+    """
+    fp = 0
+    for yt,yp in zip(y_true,y_pred):
+        if yt == 0 and yp == 1:
+            fp+=1
+    return fp
+def false_negative(y_true,y_pred):
+    """Function to calculate false negative.
+    Args:
+    -----------------
+    y_true: list of true/actual values.
+    y_pred: list of predicted values.
+
+    Returns:
+    -----------------
+    int: count of false negative.
+    """
+    fn = 0
+    for yt,yp in zip(y_true, y_pred):
+        if yt == 1 and yp == 0:
+            fn+=1
+    return fn
+
+def accuracy2(y_true,y_pred):
+    """Function to calculate accuracy using tp,tn,fp,fn.
+    """
+    tp = true_positive(y_true,y_pred)
+    tn = true_negative(y_true,y_pred)
+    fp = false_positive(y_true,y_pred)
+    fn = false_negative(y_true,y_pred)
+
+    accuracy_score = (tp+tn)/(tp+tn+fp+fn)
+    return accuracy_score

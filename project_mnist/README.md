bccccd2b17fa669edc3b75087ab61b1b551abdf2
Manisha Akshita T V <72745106+tankasalamanisha@users.noreply.github.com>
2023-06-01 08:22:47 +0000

    Started with creating folds

---
+++ project_mnist/README.md
@@ -0,0 +1,7 @@
+## MNIST digit classification model
+
+This repo will hold an implimentation of MNIST digit classification model implimented in the book: Approaching (Almost) Any Machine Learning Problem by Abhishek Thakur (@abhishekkrthakur)
+
+The intention is to learn and explore the field of Machine Learning. 
+
+Please give the book a try. Its a wonderful book to code with a cup of chai: https://store.pothi.com/book/abhishek-thakur-approaching-almost-any-machine-learning-problem/
---
+++ project_mnist/src/create_folds.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from sklearn import model_selection
+
+if __name__=="__main__":
+    data_dir = '../input/'
+
+    df = pd.read_csv(f"{data_dir}mnist_train.csv")
+
+    df['kfold'] = -1
+
+    y = df.label.values
+    
+    kf = model_selection.StratifiedKFold(n_splits=5)
+
+    for f, (t,v) in enumerate(kf.split(X=df,y=y)):
+        df.loc[v,'kfold'] = f
+
+    df.to_csv(f"{data_dir}mnist_train_folds.csv")
+    print(df.head())
+    print(df.kfold.value_counts())

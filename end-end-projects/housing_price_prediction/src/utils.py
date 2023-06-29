dcd3cace4b6fac9f9921d78130dff8f09c87e2d5
Manisha Akshita T V <72745106+tankasalamanisha@users.noreply.github.com>
2023-06-29 08:52:02 +0000

    Added the split_train_test() function

--- end-end-projects/housing_price_prediction/src/utils.py
+++ end-end-projects/housing_price_prediction/src/utils.py
@@ -2,6 +2,7 @@ import os
 import tarfile
 from six.moves import urllib
 import pandas as pd
+import numpy as np
 
 def fetch_housing_data(housing_url:str, housing_path:str):
     """Function to automatically fetch the housing data from a url and extracts the data to the given path.
@@ -19,7 +20,7 @@ def fetch_housing_data(housing_url:str, housing_path:str):
     housing_tgz.extractall(path=housing_path)
     housing_tgz.close()
 
-def load_housing_data(housing_path:str):
+def load_housing_data(housing_path:str)->pd.DataFrame:
     """Function to load the csv data
     Args:
     --------------------
@@ -29,3 +30,21 @@ def load_housing_data(housing_path:str):
     pd.DataFrame: dataframe of the loaded data."""
     csv_path = os.path.join(housing_path, "housing.csv")
     return pd.read_csv(csv_path)
+
+def split_train_test(data:pd.DataFrame, test_ratio:float):
+    """Function to split the dataset in the given ratio
+    Args:
+    ---------------
+    data: Dataframe that needs to be split into the given ratio.
+    test_ratio:Floating point to determine the test and train ratio.
+    
+    Return:
+    ------------
+    pd.DataFrame, pd.Dataframe: train dataframe, test dataframe."""
+
+    shuffled_indices = np.random.permutation(len(data))
+    test_set_size = int(len(data)*test_ratio)
+    test_indices = shuffled_indices[:test_set_size]
+    train_indices = shuffled_indices[test_set_size:]
+
+    return data.iloc[train_indices], data.iloc[test_indices]

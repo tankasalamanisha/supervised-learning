c22c8c45b0b3656c3a4f815769d706d16e7e7e85
Manisha Akshita T V <72745106+tankasalamanisha@users.noreply.github.com>
2023-12-27 07:34:40 +0000

    Completed week 1 of the course implimentation and testing

--- basics/handlers/helpers.py
+++ basics/handlers/helpers.py
@@ -1,4 +1,7 @@
 import numpy as np
+import math
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
 
 
 def generate_random_dataset(n:int,seed:int=None):
@@ -15,8 +18,8 @@ def generate_random_dataset(n:int,seed:int=None):
     """
 
     np.random.seed(seed)
-    x = 2*np.random.rand(n,1)
-    y = 4 + 3 * x + np.random.randn(n,1)
+    x = np.random.rand(n,1)
+    y = 100 + 200 * x
 
     return x,y
 
@@ -33,6 +36,193 @@ def linear_regression_model(x:float,w:float,b:float)->float:
     f(x): predicted values from the input, w and b."""
 
     return w * x +  b
+
+#Function to calculate the cost
+def compute_cost(x, y, w, b):
+   
+    m = x.shape[0] 
+    cost = 0
+    
+    for i in range(m):
+        f_wb = w * x[i] + b
+        cost = cost + (f_wb - y[i])**2
+    total_cost = 1 / (2 * m) * cost
+
+    return total_cost
+
+def compute_gradient(x, y, w, b): 
+    """
+    Computes the gradient for linear regression 
+    Args:
+      x (ndarray (m,)): Data, m examples 
+      y (ndarray (m,)): target values
+      w,b (scalar)    : model parameters  
+    Returns
+      dj_dw (scalar): The gradient of the cost w.r.t. the parameters w
+      dj_db (scalar): The gradient of the cost w.r.t. the parameter b     
+     """
+    
+    # Number of training examples
+    m = x.shape[0]    
+    dj_dw = 0
+    dj_db = 0
+    
+    for i in range(m):  
+        f_wb = w * x[i] + b 
+        dj_dw_i = (f_wb - y[i]) * x[i] 
+        dj_db_i = f_wb - y[i] 
+        dj_db += dj_db_i
+        dj_dw += dj_dw_i 
+    dj_dw = dj_dw / m 
+    dj_db = dj_db / m 
+        
+    return dj_dw, dj_db
+
+def gradient_descent(x, y, w_in, b_in, alpha, num_iters, cost_function, gradient_function): 
+    """
+    Performs gradient descent to fit w,b. Updates w,b by taking 
+    num_iters gradient steps with learning rate alpha
+    
+    Args:
+      x (ndarray (m,))  : Data, m examples 
+      y (ndarray (m,))  : target values
+      w_in,b_in (scalar): initial values of model parameters  
+      alpha (float):     Learning rate
+      num_iters (int):   number of iterations to run gradient descent
+      cost_function:     function to call to produce cost
+      gradient_function: function to call to produce gradient
+      
+    Returns:
+      w (scalar): Updated value of parameter after running gradient descent
+      b (scalar): Updated value of parameter after running gradient descent
+      J_history (List): History of cost values
+      p_history (list): History of parameters [w,b] 
+      """
+    
+    # An array to store cost J and w's at each iteration primarily for graphing later
+    J_history = []
+    p_history = []
+    b = b_in
+    w = w_in
+    
+    for i in range(num_iters):
+        # Calculate the gradient and update the parameters using gradient_function
+        dj_dw, dj_db = gradient_function(x, y, w , b)     
+
+        # Update Parameters using equation (3) above
+        b = b - alpha * dj_db                            
+        w = w - alpha * dj_dw                            
+
+        # Save cost J at each iteration
+        if i<100000:      # prevent resource exhaustion 
+            J_history.append( cost_function(x, y, w , b))
+            p_history.append([w,b])
+        # Print cost every at intervals 10 times or as many iterations if < 10
+        if i% math.ceil(num_iters/10) == 0:
+            print(f"Iteration {i}: Cost {J_history[-1]} ",
+                  f"dj_dw: {dj_dw}, dj_db: {dj_db}  ",
+                  f"w: {w}, b:{b}")
+ 
+    return w, b, J_history, p_history #return w and J,w history for graphing
+
+def inbounds(a,b,xlim,ylim):
+    xlow,xhigh = xlim
+    ylow,yhigh = ylim
+    ax, ay = a
+    bx, by = b
+    if (ax > xlow and ax < xhigh) and (bx > xlow and bx < xhigh) \
+        and (ay > ylow and ay < yhigh) and (by > ylow and by < yhigh):
+        return True
+    return False
+
+def plt_contour_wgrad(x, y, hist, ax, w_range=[-100, 500, 5], b_range=[-500, 500, 5],
+                contours = [0.1,50,1000,5000,10000,25000,50000],
+                      resolution=5, w_final=200, b_final=100,step=10 ):
+    b0,w0 = np.meshgrid(np.arange(*b_range),np.arange(*w_range))
+    z=np.zeros_like(b0)
+    for i in range(w0.shape[0]):
+        for j in range(w0.shape[1]):
+            z[i][j] = compute_cost(x, y, w0[i][j], b0[i][j] )
+
+    CS = ax.contour(w0, b0, z, contours, linewidths=2,
+                   colors=["blue", "orange", "darkred", "magenta", "purple"])
+    ax.clabel(CS, inline=1, fmt='%1.0f', fontsize=10)
+    ax.set_xlabel("w");  ax.set_ylabel("b")
+    ax.set_title('Contour plot of cost J(w,b), vs b,w with path of gradient descent')
+    w = w_final; b=b_final
+    ax.hlines(b, ax.get_xlim()[0],w, lw=2, color="purple", ls='dotted')
+    ax.vlines(w, ax.get_ylim()[0],b, lw=2, color="purple", ls='dotted')
+
+    base = hist[0]
+    for point in hist[0::step]:
+        edist = np.sqrt((base[0] - point[0])**2 + (base[1] - point[1])**2)
+        if(edist > resolution or point==hist[-1]):
+            if inbounds(point,base, ax.get_xlim(),ax.get_ylim()):
+                plt.annotate('', xy=point, xytext=base,xycoords='data',
+                         arrowprops={'arrowstyle': '->', 'color': 'r', 'lw': 3},
+                         va='center', ha='center')
+            base=point
+    return
+
+def plt_divergence(p_hist, J_hist, x_train,y_train):
+
+    x=np.zeros(len(p_hist))
+    y=np.zeros(len(p_hist))
+    v=np.zeros(len(p_hist))
+    for i in range(len(p_hist)):
+        x[i] = p_hist[i][0]
+        y[i] = p_hist[i][1]
+        v[i] = J_hist[i]
+
+    fig = plt.figure(figsize=(12,5))
+    plt.subplots_adjust( wspace=0 )
+    gs = fig.add_gridspec(1, 5)
+    fig.suptitle(f"Cost escalates when learning rate is too large")
+    #===============
+    #  First subplot
+    #===============
+    ax = fig.add_subplot(gs[:2], )
+
+    # Print w vs cost to see minimum
+    fix_b = 100
+    w_array = np.arange(-70000, 70000, 1000)
+    cost = np.zeros_like(w_array)
+
+    for i in range(len(w_array)):
+        tmp_w = w_array[i]
+        cost[i] = compute_cost(x_train, y_train, tmp_w, fix_b)
+
+    ax.plot(w_array, cost)
+    ax.plot(x,v, c="magenta")
+    ax.set_title("Cost vs w, b set to 100")
+    ax.set_ylabel('Cost')
+    ax.set_xlabel('w')
+    ax.xaxis.set_major_locator(MaxNLocator(2))
+
+    #===============
+    # Second Subplot
+    #===============
+
+    tmp_b,tmp_w = np.meshgrid(np.arange(-35000, 35000, 500),np.arange(-70000, 70000, 500))
+    z=np.zeros_like(tmp_b)
+    for i in range(tmp_w.shape[0]):
+        for j in range(tmp_w.shape[1]):
+            z[i][j] = compute_cost(x_train, y_train, tmp_w[i][j], tmp_b[i][j] )
+
+    ax = fig.add_subplot(gs[2:], projection='3d')
+    ax.plot_surface(tmp_w, tmp_b, z,  alpha=0.3, color="blue")
+    ax.xaxis.set_major_locator(MaxNLocator(2))
+    ax.yaxis.set_major_locator(MaxNLocator(2))
+
+    ax.set_xlabel('w', fontsize=16)
+    ax.set_ylabel('b', fontsize=16)
+    ax.set_zlabel('\ncost', fontsize=16)
+    plt.title('Cost vs (b, w)')
+    # Customize the view angle
+    ax.view_init(elev=20., azim=-65)
+    ax.plot(x, y, v,c="magenta")
+
+    return
 
 def mean_squared_error(y_true, y_pred)->float:
     """
@@ -54,26 +244,3 @@ def mean_squared_error(y_true, y_pred)->float:
     loss: mean cost value of the predictions."""
     loss = np.mean((y_true - y_pred))/2
     return loss
-
-def gradtient_descent(x, y , w, b, learning_rate, epochs):
-    m=len(x)
-    costs = []
-
-    for epoch in range(epochs):
-        y_pred = linear_regression_model(x, w, b)
-        cost = mean_squared_error(y, y_pred)
-        costs.append(cost)
-
-        #Compute gradients
-        dw = -(2/m) * np.sum(x* (y - y_pred))
-        db = -(2/m) * np.sum(y - y_pred)
-
-        #Update weights and biases
-        w-= learning_rate *dw
-        b -= learning_rate * db
-
-        if epoch %100 == 0:
-            print(f"Epoch {epoch}, Cost: {cost}")
-    
-    return w,b, costs
-
